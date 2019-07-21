65566b129cfd464007269ea5e3eae839fa53cee8
Vibhu <vibhu98mishra@gmail.com>
2019-07-21 13:15:54 +0530

    Added the frontend repo

---
+++ frontend/src/pages/Auth.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class AuthPage extends Component {
+    render() {
+        return (
+            <div>
+                The Auth Page
+            </div>
+        )
+    }
+}
+
+export default AuthPage;
---
+++ frontend/src/pages/Booking.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class BookingPage extends Component {
+    render() {
+        return (
+            <div>
+                BookingPage
+            </div>
+        )
+    }
+}
+
+export default BookingPage;
