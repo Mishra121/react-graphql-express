11299077aaa89094571cc84e5436fcb47896be81
Vibhu <vibhu98mishra@gmail.com>
2019-07-21 16:26:14 +0530

    MainNavigation setup  done

--- frontend/src/components/Navigation/MainNavigation.js
+++ frontend/src/components/Navigation/MainNavigation.js
@@ -1,23 +1,27 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 import './MainNavigation.css';
 
-function MainNavigation(props) {
-    return (
-        <header className="main-navigation">
-            <div className="main-navigation__logo">
-                <h1>EventHandly</h1>
-            </div>
-            <nav className="main_navigation__item">
-                <ul>
-                <li><NavLink to="/auth">Authenticate</NavLink></li>
-                    <li><NavLink to="/events">Events</NavLink></li>
-                    <li><NavLink to="/bookings">Bookings</NavLink></li>
-                </ul>
-            </nav>
-        </header>
-    )
-}
+const MainNavigation = props => (
+  <header className="main-navigation">
+    <div className="main-navigation__logo">
+      <h1>EasyEvent</h1>
+    </div>
+    <nav className="main-navigation__items">
+      <ul>
+        <li>
+          <NavLink to="/auth">Authenticate</NavLink>
+        </li>
+        <li>
+          <NavLink to="/events">Events</NavLink>
+        </li>
+        <li>
+          <NavLink to="/bookings">Bookings</NavLink>
+        </li>
+      </ul>
+    </nav>
+  </header>
+);
 
 export default MainNavigation;
