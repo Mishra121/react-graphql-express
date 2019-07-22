2d97805c7377eab2a8a3db17f9de8030f9100557
Vibhu <vibhu98mishra@gmail.com>
2019-07-22 18:34:58 +0530

    Updated Navigation

--- frontend/src/components/Navigation/MainNavigation.js
+++ frontend/src/components/Navigation/MainNavigation.js
@@ -23,9 +23,14 @@ const MainNavigation = props => (
                 <NavLink to="/events">Events</NavLink>
               </li>
               {context.token && (
+                <React.Fragment>
                 <li>
                   <NavLink to="/bookings">Bookings</NavLink>
                 </li>
+                <li>
+                  <button onClick={context.logout}>Logout</button>
+                </li>
+                </React.Fragment>
               )}
             </ul>
           </nav>
